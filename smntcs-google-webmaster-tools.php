d884e760bafefe2ec6b60fe8ae923f31d2a28d86
Niels Lange <info@nielslange.de>
2024-10-22 14:00:14 +0700

    Fix Psalm issues

--- smntcs-google-webmaster-tools.php
+++ smntcs-google-webmaster-tools.php
@@ -25,7 +25,7 @@ defined( 'ABSPATH' ) || exit;
 function smntcs_google_webmaster_tools_load_textdomain() {
 	load_plugin_textdomain( 'smntcs-google-webmaster-tools', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
-add_action( 'plugins_loaded', 'smntcs_google_webmaster_tools_load_textdomain' );
+add_action( 'plugins_loaded', 'smntcs_google_webmaster_tools_load_textdomain', 1, 0 );
 
 /**
  * Add settings link on plugin page
@@ -88,4 +88,4 @@ function smntcs_google_webmaster_tools_enqueue() {
 		print( get_option( 'smntcs_google_webmaster_tools_tracking_code' ) . "\n" ); //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 }
-add_action( 'wp_head', 'smntcs_google_webmaster_tools_enqueue' );
+add_action( 'wp_head', 'smntcs_google_webmaster_tools_enqueue', 1, 0 );
